8b0a403258f063a7f29bd55200634240af7ce43c
Pranshu Singh Bisht <106018070+pranshubisht9@users.noreply.github.com>
2023-03-31 18:02:48 +0530

    adding work

--- 04-Make-Basic-Server/index.js
+++ 04-Make-Basic-Server/index.js
@@ -2,8 +2,17 @@ const http = require('http');
 
 //we can create server
 
-http.createServer((req,res)=>{
-    res.write("hello this is pranshu");
+// http.createServer((req,res)=>{
+//     res.write("<h1>hello this is pranshu</h1>");
+//     res.end;
+// }).listen(4501);
+
+function dataController(req, res){
+        res.write("<h1>hello, this is pranshu</h1>");
     res.end;
-}).listen(4600);
+}
+
+http.createServer(dataController).listen(4502);
+
+
 
