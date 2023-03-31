d90157138b075328020a77232108526309a8ee0a
Pranshu Singh Bisht <106018070+pranshubisht9@users.noreply.github.com>
2023-03-31 18:57:39 +0530

    adding

---
+++ 05-Package_json/index.js
@@ -0,0 +1,11 @@
+// npm init -> to create package.json
+// npm install -> for installing npm
+
+//package.json: it keeps details of the priject 
+// package-lock-json: It keeps details of packages
+
+
+const colors = require('colors');
+
+
+console.log("package".bgGreen);
