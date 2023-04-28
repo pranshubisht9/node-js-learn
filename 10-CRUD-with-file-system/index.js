b5b59d425a5d2cae604da33cc93f0ebe8e975000
pranshubisht9 <pranshubisht9@gmail.com>
2023-04-28 10:19:35 +0545

    adding

--- 10-CRUD-with-file-system/index.js
+++ 10-CRUD-with-file-system/index.js
@@ -4,7 +4,26 @@ const dirPath = path.join(__dirname, 'crud');
 const filePath = `${dirPath}/apple.txt`;
 
 
-fs.writeFileSync(`${path}`, 'This is a sample text file')
-// fs.readFile(filePath, (err, item)=> {
-// console.log('vgbhn');
+//###############create file
+//Firstly i create file using below line
+// fs.writeFileSync(filePath, 'This is a sample text file')
+
+
+
+//#########read file
+// this cause showing buffer, to solve we need to put middle parameter in readFile(); which is 'utf-8'
+// fs.readFile(filePath,'utf-8', (err, item)=> {
+// console.log(item);
 // })
+
+
+// ##########update file 
+// fs.appendFile(filePath, ' and file name is apple.txt', (err) => {
+//     if(!err) console.log("file is updated");
+// })
+
+
+// #############Rename file
+fs.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
+    if(!err) console.log("file name is updated");
+})
