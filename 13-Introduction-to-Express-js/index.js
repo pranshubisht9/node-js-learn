f01c3fba199037fab3b9f0e11c2a73deebc1f7a1
pranshubisht9 <pranshubisht9@gmail.com>
2023-04-28 18:10:17 +0545

    adidng

---
+++ 13-Introduction-to-Express-js/index.js
@@ -0,0 +1,42 @@
+//################# Express js######################
+
+/* 
+
+Express.js is a popular and lightweight web framework for building web applications in Node.js. It provides a set of features and tools for creating robust and scalable web applications and APIs with minimal code. Express.js is widely used for creating RESTful APIs and web applications, and it is known for its simplicity, flexibility, and performance.
+
+ */
+
+const express = require('express');
+
+const app = express();
+
+
+// this get method provides route
+app.get()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
