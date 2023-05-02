dd154fd7e3f57c43577e127ca21c86041f71b0cd
pranshubisht9 <pranshubisht9@gmail.com>
2023-05-02 11:27:22 +0545

    adding

--- 16-Make-HTML-Page/index.js
+++ 16-Make-HTML-Page/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
+//Use of Path: It is help to access(Path) the folder in Project.
+
 
 const app = express();
 const publicPath = path.join(__dirname, 'public');
 
 app.use(express.static(publicPath));
+//This static method actually helps to load static pages,content.
 
 app.listen(5000);
